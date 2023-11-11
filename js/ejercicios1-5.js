c96172d782e2ea87efa93e83d0315376b58bd45c
angelo <aperezr@unsa.edu.pe>
2023-11-11 16:28:24 -0500

    ejercicios campus del 1 al 5 (parte 1)

---
+++ js/ejercicios1-5.js
@@ -0,0 +1,15 @@
+"use strict"
+// Ejercicio 1
+alert("un mensaje");
+// Ejercicio 2
+document.write("Hello World");
+// Ejercicio 3
+document.write("<br>"+(3+5));
+// Ejercicio 4
+let nombreUsuario = prompt("Ingrese nombre de usuario");
+document.write("<br> Hola "+nombreUsuario)
+let numero1 = parseInt(prompt("Ingrese un número"));
+let numero2 = parseInt(prompt("Ingrese otro número"));
+document.write("<br> Suma de los números: "+(numero1+numero2))
+
+
